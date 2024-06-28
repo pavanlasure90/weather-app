9b7813e2e11deb5581e6cae04636016502a6b78e
pavanlasure90 <113824942+pavanlasure90@users.noreply.github.com>
2024-06-28 12:23:11 +0530

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -256,15 +256,15 @@ export default function Home() {
 function WeatherSkeleton() {
   return (
     <section className="space-y-8 ">
-      {/* Today's data skeleton */}
+      {/* Today's data */}
       <div className="space-y-2 animate-pulse">
-        {/* Date skeleton */}
+        {/* Date  */}
         <div className="flex gap-1 text-2xl items-end ">
           <div className="h-6 w-24 bg-gray-300 rounded"></div>
           <div className="h-6 w-24 bg-gray-300 rounded"></div>
         </div>
 
-        {/* Time wise temperature skeleton */}
+        {/* Time wise temperature  */}
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
           {[1, 2, 3, 4].map((index) => (
             <div key={index} className="flex flex-col items-center space-y-2">
@@ -276,11 +276,11 @@ function WeatherSkeleton() {
         </div>
       </div>
 
-      {/* 7 days forecast skeleton */}
+      {/* 7 days forecast  */}
       <div className="flex flex-col gap-4 animate-pulse">
         <p className="text-2xl h-8 w-36 bg-gray-300 rounded"></p>
 
-        {[1, 2, 3, 4, 5, 6, 7].map((index) => (
+        {[1, 2, 3, 4, 5, 6, 7,].map((index) => (
           <div key={index} className="grid grid-cols-2 md:grid-cols-4 gap-4 ">
             <div className="h-8 w-28 bg-gray-300 rounded"></div>
             <div className="h-10 w-10 bg-gray-300 rounded-full"></div>
